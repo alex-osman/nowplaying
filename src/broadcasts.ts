c29c58efa8619e17214d178d49f0bc3173c2bfd1
Alex Osman <osman@quorum.us>
2018-01-29 18:49:28 -0500

    broadcasting votes and comments works

---
+++ src/broadcasts.ts
@@ -0,0 +1,66 @@
+import { Post } from './post'
+const steem = require('steem')
+
+const username = process.env.STEEM_USERNAME
+const wif = steem.auth.toWif(
+    username,
+    process.env.STEEM_PASSWORD,
+    'posting'
+)
+
+export const voteErrs = ['placeholder', 'Vote too small', 'already voted', 'wait 3 sec', 'max vote changes']
+
+export const vote = (post: Post, votingPower: number = 1) => {
+    return new Promise((resolve, reject) => {
+        steem.broadcast.vote(wif, username, post.author, post.permlink, votingPower * 100, (err, result) => {
+            if (err) {
+                if (err.data.code === 10) {
+                    if (err.data.stack[0].format.includes('STEEMIT_VOTE_DUST_THRESHOLD')) {
+                        // Vote too small
+                        console.log(voteErrs[1])
+                        reject(1)
+                    } else if (err.data.stack[0].format.includes('You have already voted in a similar way')) {
+                        // Already voted with this voting power
+                        console.log(voteErrs[2])
+                        reject(2)
+                    } else if (err.data.stack[0].format.includes('STEEMIT_MIN_VOTE_INTERVAL_SEC')) {
+                        // Only vote every 3 seconds
+                        console.log(voteErrs[3])
+                        setTimeout(() => vote(post, votingPower), 3000)
+                    } else if (err.data.stack[0].format.includes('STEEMIT_MAX_VOTE_CHANGES')) {
+                        console.log(voteErrs[4])
+                        reject(4)
+                    } else {
+                        console.log(err.data.stack[0].format)
+                        throw err
+                    }
+                } else {
+                    throw err
+                }
+            } else {
+                console.log('successfully voted for ', post.author)
+                resolve(result)
+            }
+        })
+    })
+}
+
+
+export const comment = (post: Post, body: string) => {
+    return new Promise((resolve, reject) => {
+        steem.broadcast.comment(wif, post.author, post.permlink, username, `nowplaying-${new Date().getTime()}`, '', `Thanks for entering this week's #nowplaying!`, { tags: ['nowplaying', 'music'], app: 'nowplaying/week5'}, (err, result) => {
+            if (err) {
+                console.log('commenting error')
+                if (err.data.code == 10) {
+                    // Only comment every 20 seconds
+                    reject(1)
+                } else {
+                    reject(err)
+                }
+            } else {
+                console.log('successfully commented on ', post.author)
+                resolve(result)
+            }
+        })
+    })
+}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 const ncp = require('copy-paste')
 import {
-  weekFilter,
-  commentFilter
+  weekFilter
 } from './filters'
 import {
   reportStartWeek
@@ -12,6 +11,7 @@ import {
   writePosts,
 } from './data';
 import { Post } from './post';
+import { vote, voteErrs } from './broadcasts';
 
 const mysql = require('promise-mysql');
 
@@ -26,16 +26,23 @@ const local = {
 
 const main = async () => {
   const con = await mysql.createConnection(local)
-  const users = await getUsers(con)
+  const post = { author: 'walnut1', 'permlink': 'steemit-open-mic-week-69-grateful-dead-casey-jones' } as Post
+  const body = 'Test Comment1'
+  try {
+    const voteResponse = await vote(post, 25)
+  } catch(e) {
+    // console.log(voteErrs[e])
+  }
+  // const users = await getUsers(con)
   // const posts = await getPosts() as Post[]
   // const write = await writePosts(con, posts)
   // const comment = await commentPosts(con, posts.filter(commentFilter))
   // const vote = await commentPosts(con, posts.filter(voteFilter))
 
-  const report = reportStartWeek(users.filter(weekFilter(4)))
+  // const report = reportStartWeek(users.filter(weekFilter(4)))
 
-  console.log(report)
-  ncp.copy(report, () => console.log('Copied to clipboard'))
+  // console.log(report)
+  // ncp.copy(report, () => console.log('Copied to clipboard'))
 }
 
 main()
