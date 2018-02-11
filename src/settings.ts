fb4582fe1a27119ba5fb237380064afdf38ba809
Alex Osman <osman@quorum.us>
2018-02-11 11:11:20 -0500

    make the settings take care of everything

--- src/settings.ts
+++ src/settings.ts
@@ -1,8 +1,11 @@
-export const Settings = {
+export const settings = {
+    communityName: 'nowplaying',
+    username: process.env.STEEM_USERNAME,
+    password: process.env.STEEM_PASSWORD,
     week: 6,
     blacklist: [
         'nowplaying-music',
-        
+
         'arsaljr',
         'clicknpict',
         'dianasteem7',
