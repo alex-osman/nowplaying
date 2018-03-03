8258c4a69611f51d66932de0445dc9df5f363beb
Alex Osman <osman@quorum.us>
2018-03-03 15:48:47 -0500

    got rid of the extra print statements

--- src/process/postRecap.ts
+++ src/process/postRecap.ts
@@ -5,7 +5,7 @@ const ncp = require('copy-paste')
 const main = async () => {
     const bot: Bot = await init()
     const report = await bot.postRecap()
-    console.log(report)
+    // console.log(report)
     ncp.copy(report.post.body, () => console.log('Copied to clipboard'))
 }
 
