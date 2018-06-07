c890d1a0f2fa7fc71f61c2dc104101dac909e534
Alex Osman <alex@Alexs-MacBook-Pro.local>
2018-06-06 21:07:57 -0400

    bot.scrape given correct context

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { Bot } from "./bot";
 const main = async () => {
   const bot: Bot = await init();
   
-  setInterval(bot.scrape, 1000 * 60 * 30)
+  setInterval(() => bot.scrape(), 1000 * 60 * 30)
   bot.scrape()
 }
 main()
